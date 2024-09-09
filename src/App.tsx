ff762c4c85ed9592b35895cf2df3f91007dfc09c
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2024-09-09 12:01:25 -0700

    feat: add material ui comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider, type createRouter } from "@tanstack/react-router";
 import type { FunctionComponent } from "./common/types";
-// import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
+import { Container, Box, Typography, Link } from '@mui/material';
 
 const queryClient = new QueryClient();
 
 type AppProps = { router: ReturnType<typeof createRouter> };
 
+function Copyright(): JSX.Element {
+  return (
+    <Typography
+      align="center"
+      variant="body2"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Chris Rubio
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
 const App = ({ router }: AppProps): FunctionComponent => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
-			{/* <TanStackRouterDevelopmentTools
-				router={router}
-				initialIsOpen={false}
-				position="bottom-right"
-			/>
-			<ReactQueryDevtools initialIsOpen={false} /> */}
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Copyright />
+        </Box>
+      </Container>
 		</QueryClientProvider>
 	);
 };
