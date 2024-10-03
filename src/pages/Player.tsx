7ef11428ee0e7bbc5e19d5bc81599bffc3f23d88
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2024-10-03 10:57:32 -0700

    feat: player stats display components

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,7 +2,8 @@ import { useParams, useSearchParams } from "react-router-dom";
 import type { FunctionComponent } from "../common/types";
 import { useQuery } from "@tanstack/react-query";
 import { getPlayer, getPlayerByTeam, SORT_TYPE } from "../features/player-instance/api";
-import { Container } from "@mui/material";
+import { Container, Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import type { PlayerStats, PlayerByTeamStats } from "../features/player-instance/types";
 
 export const Player = (): FunctionComponent => {
   const parameters = useParams()
@@ -16,24 +17,109 @@ export const Player = (): FunctionComponent => {
     enabled: !!playerId && !sortByTeam
   });
 
-  const {data: playerByTeamData, isLoading: isPlayerByTeamLoading} = useQuery({
+  const {data: playerByTeamData, isLoading: isPlayerTeamDataLoading} = useQuery({
     queryKey: ['player-by-team', playerId],
     queryFn: async () => playerId ? getPlayerByTeam(playerId, {sort: SORT_TYPE.TEAM}) : Promise.reject(new Error('Player ID is undefined')),
-    enabled: !!playerId && sortByTeam
+    enabled: true
   });
+
+  function renderStatsByTeam(stats: Record<string, PlayerByTeamStats>): JSX.Element {
+    const teamStats = [];
+    for (const key in stats) {
+      teamStats.push(
+        <TableRow>
+          <TableCell>{key}</TableCell>
+          <TableCell>{stats[key]?.GP}</TableCell>
+          <TableCell>{stats[key]?.G}</TableCell>
+          <TableCell>{stats[key]?.A}</TableCell>
+          <TableCell>{stats[key]?.PIM}</TableCell>
+          <TableCell>{stats[key]?.PTS}</TableCell>
+          <TableCell>{stats[key]?.PPG}</TableCell>
+          <TableCell>{stats[key]?.SHG}</TableCell>
+        </TableRow>
+      );
+    }
+    
+    return (<>{teamStats}</>);
+  }      
+
+  function renderStats(stats: PlayerStats): JSX.Element {
+    return (
+      <TableRow>
+        <TableCell>{stats.games_played}</TableCell>
+        <TableCell>{stats.goals}</TableCell>
+        <TableCell>{stats.assists}</TableCell>
+        <TableCell>{stats.penalty_minutes}</TableCell>
+        <TableCell>{stats.points}</TableCell>
+        <TableCell>{stats.powerplay_goals}</TableCell>
+        <TableCell>{stats.shorthanded_goals}</TableCell>
+      </TableRow>
+    );
+  }
 
   return (
     <div>
-      <p>Player ID: {playerId}</p>
-      {isPlayerDataLoading || isPlayerByTeamLoading && (
-        <p>Loading...</p>
+      {(isPlayerDataLoading || isPlayerTeamDataLoading) && (
+        <p>Loading stats...</p>
       )}
       <Container maxWidth="sm">
         {playerData && (
-          JSON.stringify(playerData)
-        )}
-        {playerByTeamData && (
-          JSON.stringify(playerByTeamData)
+          <>
+            <Paper elevation={0}  sx={{marginBottom: '8px', padding: '4px'}}>
+              <Typography variant="h5" >
+                <Link href={playerData.stats_url}>
+                {playerData.full_name} - {playerId}
+                </Link>
+              </Typography>
+            </Paper>
+            <Paper sx={{marginBottom: '8px', padding: '4px'}}>
+              <Typography variant="h4">
+                Lifetime stats
+              </Typography>
+              <TableContainer>
+                <Table sx={{ minWidth: '100%'}}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>GP</TableCell>
+                      <TableCell>G</TableCell>
+                      <TableCell>ASS</TableCell>
+                      <TableCell>PIM</TableCell>
+                      <TableCell>PTS</TableCell>
+                      <TableCell>PPG</TableCell>
+                      <TableCell>SHG</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {renderStats(playerData.stats)}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
+            <Paper sx={{marginBottom: '8px', padding: '4px'}}>
+              <Typography variant="h4">
+                Team stats
+              </Typography>
+              <TableContainer>
+                <Table sx={{ minWidth: '100%'}}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Team</TableCell>
+                      <TableCell>GP</TableCell>
+                      <TableCell>G</TableCell>
+                      <TableCell>ASS</TableCell>
+                      <TableCell>PIM</TableCell>
+                      <TableCell>Points</TableCell>
+                      <TableCell>PPG</TableCell>
+                      <TableCell>SHG</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {playerByTeamData?.team_stats && renderStatsByTeam(playerByTeamData.team_stats)}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
+          </>
         )}
       </Container>
     </div>
